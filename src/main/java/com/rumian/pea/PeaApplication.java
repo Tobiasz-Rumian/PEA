cf67baa48ef12aa0498f1c796ea7ec007e5dad9d
Tobiasz Rumian <tobiasz.rumian@hotmail.com>
2017-10-18 15:10:56 +0200

    Initial commit

---
+++ src/main/java/com/rumian/pea/PeaApplication.java
@@ -0,0 +1,12 @@
+package com.rumian.pea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaApplication.class, args);
+	}
+}
